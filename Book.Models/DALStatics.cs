633cf66cd04d73e1bd8aee4b9cc71ff6e5c902f5
Om357 <109865834+Om357@users.noreply.github.com>
2024-06-07 16:32:43 +0530

    Add files via upload

---
+++ Book.Models/DALStatics.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer
+{
+    public class DALStatics
+    {
+        public static List<UserInfo> Users = new List<UserInfo>()
+        {
+            new UserInfo()
+            {
+                Username = "om_admin",
+                EmailAddress = "om.admin@tatvasoft.com",
+                Password = "Admin_pwd123",
+                GivenName = "Om",
+                Surname = "Rathod",
+                Role = "Administrator"
+            },
+
+            new UserInfo()
+            {
+                Username = "john_reader",
+                EmailAddress = "john.user@tatvasoft.com",
+                Password = "john_pwd",
+                GivenName = "John",
+                Surname = "Doe",
+                Role = "BookUser"
+            },
+        };
+    }
+}
